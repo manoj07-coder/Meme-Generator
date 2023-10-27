c52a6422c49ca1b81b42c49085802b41ccf9b1c5
Manoj <manojgowdacs35@gmail.com>
2023-10-27 23:27:46 +0530

    App.css updated

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Header(){
+    return (
+        <header className ="header">
+            <img src='../public/troll-face.png' className='header-img' />
+            <h2 className='header-title'>Meme Generator</h2>
+            
+        </header>
+    )
+}
